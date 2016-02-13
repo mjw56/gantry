ed1c349403e1f1e58a5ba44cd67c8fcde8902176
Mike Wilcox <mwilcox56@gmail.com>
2016-02-12 21:46:17 -0500

    node filesystem stuff

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
 var chalk = require('chalk');
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 program
   .option('-t, --type <app>', 'The application type to install')
@@ -18,4 +21,25 @@ program
 if (program.type) {
   console.log('user entered type', program.type)
   // TODO: create function to move files to users local directory
+  moveApp(program.type);
+}
+
+function moveApp(type) {
+  console.log('gantry location:' + path.resolve(__dirname) + ' users location ' + path.resolve('.'));
+  
+  mkdirp(path.join(path.resolve('.'), 'react'), function (err) {
+    if (err) console.error(err)
+    else console.log('pow!')
+  });
+
+  var source = fs.createReadStream(path.join(path.resolve(__dirname), 'lib', 'react'));
+  var dest = fs.createWriteStream(path.join(path.resolve('.'), 'react'));
+
+  source.pipe(dest);
+  source.on('end', function() { 
+    console.log('files copied');
+  });
+  source.on('error', function(err) {
+    console.log('error!');
+  });
 }
