f7acafcd68826d2e0a937486ae95c55febdb8cdf
Mike Wilcox <mwilcox56@gmail.com>
2016-02-15 21:23:58 -0500

    move questions logic into its own file

--- src/questions.js
+++ src/questions.js
@@ -1,58 +1,13 @@
 import inquirer from 'inquirer';
-import { transfer } from './transfer';
+import { install } from './install';
+import config from './configs/questions';
 
-exports.questions = function questions(type) {
-  // TODO: these questions will be based on type
-  inquirer.prompt([
-    {
-      when: function(name){ return name; },
-      type: 'input',
-      name: 'name',
-      message: 'Would you like to name your app?',
-      default: 'prototype'
-    },
-    {
-      when: function(name){ return name; },
-      type: 'input',
-      name: 'description',
-      message: 'What is the description of the app?',
-      default: 'A new prototype'
-    },
-    {
-      type: 'confirm',
-      name: 'rxjs',
-      message: 'Would you like to use ESLint?',
-      default: true
-    },
-    {
-      type: 'confirm',
-      name: 'tests',
-      message: 'Would you like to include tests?',
-      default: true
-    },
-    {
-      type: 'confirm',
-      name: 'typescript',
-      message: 'Would you like to use TypeScript?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'immutable',
-      message: 'Would you like to use ImmutableJS?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'redux',
-      message: 'Would you like to use Redux?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'rxjs',
-      message: 'Would you like to use RxJS?',
-      default: false
-    }
-  ], transfer.bind(this, type))
+const questions = function questions(type) {
+  // TODO: these questions should be based on type
+  inquirer.prompt(
+    config.app.concat(config.tools[type]), 
+    install.bind(this, type)
+  );
 }
+
+export { questions };
