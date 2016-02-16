af739e1f66c3b1e35c154d093348520190932f65
Mike Wilcox <mwilcox56@gmail.com>
2016-02-15 21:23:02 -0500

    refactor/cleanup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 import program from 'commander';
 import inquirer from 'inquirer';
 import {questions} from './questions';
+import blessed from 'blessed';
+import contrib from 'blessed-contrib';
 
 program
   .option('-t, --type <app>', 'The application type to install')
