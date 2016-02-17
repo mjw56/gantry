4522407d9a74a9eef2f0fea75bd73f0ddaacc748
Mike Wilcox <mwilcox56@gmail.com>
2016-02-16 21:06:53 -0500

    update devDependencies

--- src/configs/angular.js
+++ src/configs/angular.js
@@ -1,6 +1,13 @@
 const config = {
   dependencies: {
     angular: 'angular'
+  },
+  devDependencies: {
+    babel: 'babel',
+    babelPresetEs2015: 'babel-preset-es2015',
+    babelify: 'babelify',
+    browserify: 'browserify',
+    liteServer: 'lite-server'
   }
 };
 
