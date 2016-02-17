b039e10b46ed5d62802ef757f0096422e5cb4610
Mike Wilcox <mwilcox56@gmail.com>
2016-02-16 20:26:30 -0500

    add babel presets

--- templates/config/webpack/react/webpack.config.js
+++ templates/config/webpack/react/webpack.config.js
@@ -26,9 +26,12 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: [ 'babel' ],
-        exclude: /node_modules/,
-        include: __dirname
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        include: [__dirname],
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
     ]
   }
