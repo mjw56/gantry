0db405bbbea508c9bd2994522f91ba1e3d5efda5
Mike Wilcox <mwilcox56@gmail.com>
2016-02-18 21:31:26 -0500

    simplify

--- src/transfer.js
+++ src/transfer.js
@@ -5,17 +5,10 @@ import { Promise } from 'es6-promise';
 var transfer = function transfer(config, vars) {
   return new Promise((resolve, reject) => {
     const {
-      bundler,
-      folder,
       library
     } = config;
 
-    let opts;
-    if (folder === 'project') {
-      opts = ['../', 'templates', library];
-    } else if (folder === 'config') {
-      opts = ['../', 'templates', 'config', bundler.toLowerCase(), library];
-    }
+    const opts = ['../', 'templates', library];
 
     copy(
       path.join(path.resolve(__dirname), ...opts),
