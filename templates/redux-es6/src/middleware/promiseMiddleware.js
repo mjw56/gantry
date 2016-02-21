7cee7daaa2974437cc467bf2729aac591986e6e8
Mike Wilcox <mwilcox56@gmail.com>
2016-02-21 07:40:16 -0500

    use the type key

--- templates/redux-es6/src/middleware/promiseMiddleware.js
+++ templates/redux-es6/src/middleware/promiseMiddleware.js
@@ -1,18 +1,20 @@
 export default function promiseMiddleware() {
   return next => action => {
-    const { promise, types, ...rest } = action;
+    const { promise, type, ...rest } = action;
 
     if (!promise) return next(action);
 
-    next({ ...rest, type: types[0] });
+    next({ ...rest, type: type[0] });
 
     return promise
       .then(res => {
-        next({ ...rest, res, type: types[1] });
-        return true;
+        res.json().then((r) => {
+          next({ ...rest, data: r, type: type[1] });
+          return true;
+        });
       })
       .catch(error => {
-        next({ ...rest, error, type: types[2] });
+        next({ ...rest, error, type: type[2] });
         return false;
       });
   };
